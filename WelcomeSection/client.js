72e79179372722c0f2c2a4771e2d34922814c3df
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-02-13 17:58:24 -0700

    button ripple effect starter

--- WelcomeSection/client.js
+++ WelcomeSection/client.js
@@ -1,1 +1,13 @@
-document.body.style.background = "lightblue";
+//toggle button animation 
+const btnEffect = document.querySelector('a');
+btnEffect.forEach(button => {
+  button.addEventlistener('click', function(e) {
+    let btnX = e.clientX - e.target.offsetLeft;
+    let btnY = e.clientY - e.target.offsetTop;
+
+    let ripEffect = document.getElementById('btnEffect');
+    ripEffect.style.left = x + 'px';
+    ripEffect.style.top = y + 'px';
+    this.appendChild(ripEffect);
+  });
+})
