d0d5605af10746c97d7b5ecf4bc513eadc7e1446
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-02-13 19:43:03 -0700

    add loading page file

--- WelcomeSection/client.js
+++ WelcomeSection/client.js
@@ -1,17 +1,4 @@
-//toggle button animation 
-const btnEffect = document.querySelector('button');
-btnEffect.forEach(button => {
-  button.addEventlistener('click', function(e) {
-    let btnX = e.clientX - e.target.offsetLeft;
-    let btnY = e.clientY - e.target.offsetTop;
-
-    let ripEffect = document.getElementById('btnEffect');
-    ripEffect.style.left = x + 'px';
-    ripEffect.style.top = y + 'px';
-    this.appendChild(ripEffect);
-
-    setTimeout(() => {
-      ripEffect.remove()
-    }, 1000);
-  });
-})
+function toggleEvent() {
+  const btn = document.body;
+  btn.classList.toggle("darkBtn");
+}
