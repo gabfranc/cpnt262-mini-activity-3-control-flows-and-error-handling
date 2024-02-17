b64c22677f9acab8f51e26966827c0d4395042fd
Gabrielle Ann Franco <156222595+gabfranc@users.noreply.github.com>
2024-02-17 14:14:26 -0700

    Add files via upload

---
+++ client.js
@@ -0,0 +1,13 @@
+// toggle switch will turn body background light and turn to another page.
+"use strict";
+
+const btn = document.body;
+
+let btnText = "hello universe!";
+let txtStr = btnText.toUpperCase();
+
+document.getElementById("btnText").innerHTML = txtStr;
+
+function toggleEvent() {
+  btn.classList.toggle("lightBtn");
+}
